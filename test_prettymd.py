4aa60583c821bcb6d9db43cf3a1862bf72d93608
Akash B <akash.bhattacharya.18@ucl.ac.uk>
2020-03-19 23:10:37 +0000

    Tests - to be implemented

---
+++ test_prettymd.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+from .prettymd import PrettyMD
+
+
+class TestPrettyMD(TestCase):
+    def test_parse_headings(self):
+
+
+
+        return
+
+    def test_make_toc(self):
+        self.fail()
+
+    def test_flatten_contents_tree(self):
+        self.fail()
+
+    def test_insert_anchors(self):
+        self.fail()
+
+    def test_get_heading_level(self):
+        self.fail()
+
+    def test_construct_contents_tree(self):
+        self.fail()
+
+    def test_toc(self):
+        self.fail()
+
+    def test_output(self):
+        with open("./markdowns/input_0.md") as file:
+            md_text = file.read()
+        with open("./markdowns/output_0.md") as file:
+            expected_output = file.read()
+
+        p = PrettyMD(md_text, includes_title=False, link=True)
+
+        self.assertEqual(p.output, expected_output)
+        return
